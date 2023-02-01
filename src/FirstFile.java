b19b8e797d613073540b6a1479bc2ef3a9193aad
sunnygnkr <sunnygnkr93@gmail.com>
2023-02-01 17:23:40 +0530

    Code added for generating random number upto 10

--- src/FirstFile.java
+++ src/FirstFile.java
@@ -3,8 +3,13 @@ public class FirstFile {
 
 	public static void main(String[] args) {
 		System.out.println("Adding first file to the local git repo");
-		
+
 		System.out.println("This line was added from Github");
-		
+
+		int i = 0;
+		while (i < 5) {
+			System.out.println(Math.round(Math.random() * 10));
+			i++;
+		}
 	}
 }
