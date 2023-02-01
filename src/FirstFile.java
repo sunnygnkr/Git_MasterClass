1eebf5b1e0cb5746d87092bbfbf5326d34e73596
Sunny Gaonkar <sunnygnkr93@gmail.com>
2023-02-01 15:32:35 +0530

    Update FirstFile.java
    Updated and committed the file directly through github.

--- src/FirstFile.java
+++ src/FirstFile.java
@@ -3,5 +3,8 @@ public class FirstFile {
 
 	public static void main(String[] args) {
 		System.out.println("Adding first file to the local git repo");
+		
+		System.out.println("This line was added from Github");
+		
 	}
 }
