4bf7f3d610b6481dbd8d2e3e0d094e8685d91a7b
sunnygnkr <sunnygnkr93@gmail.com>
2023-02-06 12:19:12 +0530

    This array program class is added to new function1 branch

---
+++ src/ArrayIntro.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+@SuppressWarnings("unused")
+public class ArrayIntro {
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[4];
+		
+		for(int val:arr) {
+			System.out.println(val);
+		}
+	}
+}
